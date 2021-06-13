736ef0dbd63846d2c271562bbdf792228087f1c5
LoTwT <709937065@qq.com>
2021-06-13 12:25:55 +0800

    router init

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  This is Home!!
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+export default defineComponent({
+  name: "Home",
+  setup: () => {
+
+  }
+})
+</script>
